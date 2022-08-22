7567fb33e0b12f5f9f878bc6473e5146d083b3f9
Dara16 <just4oludee@yahoo.com>
2022-08-21 23:49:40 -0400

    homepage image added

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,8 +4,11 @@ import React from 'react';
 
 export default function Homepage() {
     return (
-        <div>
-
+        <div class="homepage">
+            <div class="profile-image"> 
+                <img src="../media/joey-pedras-oLICUNlh_OM-unsplash.jpg" alt="background"></img>
+            </div>
+            <h1>CONSTELLATION PROPERTY MANAGEMENT LLC</h1>
         </div>
     )
 }
